efed20c250fdf389fe11289a688052e3b1cff6e1
Maxim Zebzeev <maksimzeb@yandex.ru>
2020-12-12 12:42:45 +0500

    Added function validate form not work

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,22 @@
 "use strict"
+// Проверка формы 
+function validateForm() {
+	let x = document.forms["myForm"]["frame"].value;
+	if (x == "") {
+		alert("Форму надо заполнить и указать имя и фамилию");
+		return false;
+	}
+}
+// Проверка длинны номера телефона
+function validateTel () {
+	let x, text;
+	
+	// сюда приходят значения от пользователей
+	x = document.getElementById("tel").value;
+
+	// провека djlbvsq lfyys[]
+	if (isNaN(x) && x === 11 ) {
+		text = "Номер должен быть из 11 цифр и без букв";
+	}
+	document.getElementById("demo").innerHTML = text;
+}
